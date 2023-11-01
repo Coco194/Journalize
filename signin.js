895fd01debea87215a75a9bd85c86152427e2297
Mohamed Shaffaq <mohamedshaffaq@gmail.com>
2023-11-01 19:56:51 +0530

    Create signin.js
    Js for sign in page

---
+++ signin.js
@@ -0,0 +1,26 @@
+//Setting the right-arrow image visibility 
+function emailelementin(){
+    document.getElementById("email").style.opacity = 1;
+}
+
+function emailelementout(){
+    document.getElementById("email").style.opacity = 0;
+}
+
+function googleelementin(){
+    document.getElementById("google").style.opacity = 1;
+}
+
+function googleelementout(){
+    document.getElementById("google").style.opacity = 0;
+}
+
+function facebookelementin(){
+    document.getElementById("facebook").style.opacity = 1;
+}
+
+function facebookelementout(){
+    document.getElementById("facebook").style.opacity = 0;
+}
+//end
+
